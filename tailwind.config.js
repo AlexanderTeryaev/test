e47469fd0a756c825757b52e922a6e49bd3d5aa2
Jin <webDev428@gmail.com>
2021-02-17 05:01:45 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontSize: {
+        50: '3.125rem',
+      },
+      height: {},
+      width: {},
+      minWidth: {},
+      margin: {},
+      body: ['"TypoGrotesk"'],
+      display: ['TypoGrotesk'],
+      fontFamily: {
+        typoGrotesk: 'TypoGrotesk'
+      },
+      colors: {
+        purple1: '#6948B7',
+        purple2: '#5230A2',
+        gray5: '#181A1D',
+        lightblue: '#44e0b7',
+      }
+    },
+    screens: {
+      'max-3xl': { max: '1920px' },
+      // => @media (max-width: 1920px) { ... }
+
+      'max-2xl': { max: '1535px' },
+      // => @media (max-width: 1535px) { ... }
+
+      'max-xl': { max: '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      'max-lg': { max: '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      'max-md': { max: '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      'max-sm': { max: '639px' }
+      // => @media (max-width: 639px) { ... }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
